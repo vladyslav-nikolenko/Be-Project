b852289f3c71b236f8985861beaf973295da73cb
Pavel Nikitsiuk <p.nikitsiuk@godeltech.com>
2022-07-11 18:44:18 +0300

    add ci scripts

--- deploy/sh/deploy-dev.sh
+++ deploy/sh/deploy-dev.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -xe
 
-DATABASE_URL=${DATABASE_URL_DEV} npx prisma migrate dev --name init
-
 image=$( cat image )
 
 sh ${SHARED_SCRIPTS_DIR}/sh/deploy-argocd.sh \
--- deploy/sh/deploy-prod.sh
+++ deploy/sh/deploy-prod.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -xe
 
-DATABASE_URL=${DATABASE_URL_PROD} npx prisma migrate deploy
-
 image=$( cat image )
 
 sh ${SHARED_SCRIPTS_DIR}/sh/deploy-argocd.sh \
