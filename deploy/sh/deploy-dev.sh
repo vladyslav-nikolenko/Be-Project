8c4e2bea27562195f3e20a31baf6e5e3b32542fc
Pavel Nikitsiuk <p.nikitsiuk@godeltech.com>
2022-07-13 16:38:11 +0300

    update deploy path

--- deploy/sh/deploy-dev.sh
+++ deploy/sh/deploy-dev.sh
@@ -4,6 +4,6 @@ set -xe
 image=$( cat image )
 
 sh ${SHARED_SCRIPTS_DIR}/sh/deploy-argocd.sh \
-  -p "applications/dev/lnd/news/news.yaml" \
+  -p "applications/dev/lnd/news-service/news-service.yaml" \
   -k "deploy/k8s/dev" \
   -i $image
--- deploy/sh/deploy-prod.sh
+++ deploy/sh/deploy-prod.sh
@@ -4,6 +4,6 @@ set -xe
 image=$( cat image )
 
 sh ${SHARED_SCRIPTS_DIR}/sh/deploy-argocd.sh \
-  -p "applications/prod/lnd/news/news.yaml" \
+  -p "applications/prod/lnd/news-service/news-service.yaml" \
   -k "deploy/k8s/prod" \
   -i $image
